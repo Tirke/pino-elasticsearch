2fe35ebd5381391e841fe09dcfd4d86f4dd16e13
Tirke <tirke@tirke.dev>
2022-05-17 20:34:22 +0200

    test

--- lib.js
+++ lib.js
@@ -1,115 +1,89 @@
-'use strict'
-
-/* eslint no-prototype-builtins: 0 */
-
-const split = require('split2')
-const { Client, Connection } = require('@elastic/elasticsearch')
-
-function pinoElasticSearch (opts) {
-  if (opts['bulk-size']) {
-    process.emitWarning('The "bulk-size" option has been deprecated, "flush-bytes" instead')
-    delete opts['bulk-size']
-  }
-
-  const splitter = split(function (line) {
-    let value
-
-    try {
-      value = JSON.parse(line)
-    } catch (error) {
-      this.emit('unknown', line, error)
-      return
-    }
-
-    if (typeof value === 'boolean') {
-      this.emit('unknown', line, 'Boolean value ignored')
-      return
-    }
-    if (value === null) {
-      this.emit('unknown', line, 'Null value ignored')
-      return
-    }
-    if (typeof value !== 'object') {
-      value = {
-        data: value,
-        time: setDateTimeString(value)
-      }
-    } else {
-      if (value['@timestamp'] === undefined) {
-        value.time = setDateTimeString(value)
-      }
-    }
-
-    function setDateTimeString (value) {
-      if (typeof value === 'object' && value.hasOwnProperty('time')) {
-        if (
-          (typeof value.time === 'string' && value.time.length) ||
-          (typeof value.time === 'number' && value.time >= 0)
-        ) {
-          return new Date(value.time).toISOString()
-        }
-      }
-      return new Date().toISOString()
-    }
-    return value
-  }, { autoDestroy: true })
-
-  const client = new Client({
-    node: opts.node,
-    auth: opts.auth,
-    cloud: opts.cloud,
-    ssl: { rejectUnauthorized: opts.rejectUnauthorized },
-    Connection: opts.Connection || Connection
-  })
-
-  const esVersion = Number(opts['es-version']) || 7
-  const index = opts.index || 'pino'
-  const buildIndexName = typeof index === 'function' ? index : null
-  const type = esVersion >= 7 ? undefined : (opts.type || 'log')
-  const opType = esVersion >= 7 ? opts.op_type : undefined
-  const b = client.helpers.bulk({
-    datasource: splitter,
-    flushBytes: opts['flush-bytes'] || 1000,
-    flushInterval: opts['flush-interval'] || 30000,
-    refreshOnCompletion: getIndexName(),
-    onDocument (doc) {
-      const date = doc.time || doc['@timestamp']
-      if (opType === 'create') {
-        doc['@timestamp'] = date
-      }
-
-      return {
-        index: {
-          _index: getIndexName(date),
-          _type: type,
-          op_type: opType
-        }
-      }
-    },
-    onDrop (doc) {
-      const error = new Error('Dropped document')
-      error.document = doc
-      splitter.emit('insertError', error)
-    }
-  })
-
-  b.then(
-    (stats) => splitter.emit('insert', stats),
-    (err) => splitter.emit('error', err)
-  )
-
-  splitter._destroy = function (err, cb) {
-    b.then(() => cb(err), (e2) => cb(e2 || err))
-  }
-
-  function getIndexName (time = new Date().toISOString()) {
-    if (buildIndexName) {
-      return buildIndexName(time)
-    }
-    return index.replace('%{DATE}', time.substring(0, 10))
-  }
-
-  return splitter
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const elasticsearch_1 = require("@elastic/elasticsearch");
+const split2_1 = tslib_1.__importDefault(require("split2"));
+class DropDocumentError extends Error {
 }
-
-module.exports = pinoElasticSearch
+function pinoElasticSearch(opts) {
+    const splitter = (0, split2_1.default)(function (line) {
+        let value;
+        const setDateTimeString = (value) => {
+            if (typeof value === 'object' && value.hasOwn('time')) {
+                if ((typeof value.time === 'string' && value.time.length) ||
+                    (typeof value.time === 'number' && value.time >= 0)) {
+                    return new Date(value.time).toISOString();
+                }
+            }
+            return new Date().toISOString();
+        };
+        try {
+            value = JSON.parse(line);
+        }
+        catch (error) {
+            this.emit('unknown', line, error);
+            return;
+        }
+        if (typeof value === 'boolean') {
+            // eslint-disable-next-line no-invalid-this
+            this.emit('unknown', line, 'Boolean value ignored');
+            return;
+        }
+        if (value === null) {
+            this.emit('unknown', line, 'Null value ignored');
+            return;
+        }
+        if (typeof value !== 'object') {
+            value = {
+                data: value,
+                time: setDateTimeString(value),
+            };
+        }
+        else if (value['@timestamp'] === undefined) {
+            value.time = setDateTimeString(value);
+        }
+        return value;
+    }, { autoDestroy: true });
+    const client = new elasticsearch_1.Client({
+        node: opts.node,
+        auth: opts.auth,
+        cloud: opts.cloud,
+    });
+    const index = opts.index || 'pino';
+    const type = opts.esVersion >= 7 ? undefined : opts.type || 'log';
+    const opType = opts.esVersion >= 7 ? opts.op_type : undefined;
+    const getIndexName = (time = new Date().toISOString()) => {
+        return index.replace('%{DATE}', time.substring(0, 10));
+    };
+    const b = client.helpers.bulk({
+        datasource: splitter,
+        flushBytes: opts.flushBytes || 1000,
+        flushInterval: opts.flushInterval || 30000,
+        refreshOnCompletion: getIndexName(),
+        onDocument(doc) {
+            const date = doc.time || doc['@timestamp'];
+            if (opType === 'create') {
+                doc['@timestamp'] = date;
+            }
+            return {
+                index: {
+                    _index: getIndexName(date),
+                    _type: type,
+                    op_type: opType,
+                },
+            };
+        },
+        onDrop(doc) {
+            const error = new DropDocumentError('Dropped document');
+            error.document = doc;
+            splitter.emit('insertError', error);
+        },
+    });
+    b.then((stats) => splitter.emit('insert', stats), (err) => splitter.emit('error', err));
+    splitter._destroy = function (err, cb) {
+        b.then(() => cb(err), (e2) => cb(e2 || err));
+    };
+    return splitter;
+}
+exports.default = pinoElasticSearch;
+//# sourceMappingURL=index.js.map
